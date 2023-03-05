f555a02be33ce6b7e7628151177964b5f4a19946
TallFly <tallfly@vk.com>
2023-03-05 10:44:23 +0600

    добавлены команды для IP и порта

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import asyncio
 from mcstatus import JavaServer
 
 TOKEN = input("Введите свой токен: ")
-SERVER_IP = input("Введите IP сервера: ")
-SERVER_PORT = input("Введите порт сервера (стандарт 25565): ")
+SERVER_IP = None
+SERVER_PORT = 25565
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 server = JavaServer.lookup(f'{SERVER_IP}:{SERVER_PORT}')
@@ -24,9 +25,9 @@ async def update_presence():
     while not bot.is_closed():
         try:
             status = server.status()
-            await bot.change_presence(activity=discord.Game(name=f"{status.players.online} игроков онлайн"))
+            await bot.change_presence(activity=discord.Game(name=f"{status.players.online}/{status.players.max} игроков онлайн"))
         except:
-            await bot.change_presence(activity=discord.Game(name="Тех.обслуживание"))
+            await bot.change_presence(activity=discord.Game(name="Введите IP и порт сервера"))
         await asyncio.sleep(30)
 
 @bot.tree.command(name="online")
@@ -38,7 +39,27 @@ async def online(interaction: discord.Interaction):
             max_players = status.players.max
             response = f"**Сейчас на сервере {online}/{max_players} игроков:**\n{players}"
         except:
-            response = "**Сервер на тех.обслуживании**"
+            response = "**Сейчас игроков нет**"
         await interaction.response.send_message(response)
+
+@bot.tree.command(name="ip", description="Добавление IP адреса сервера")
+@app_commands.describe(ip = "IP адрес сервера")
+async def ip(interaction: discord.Interaction, ip: str):
+    global SERVER_IP
+    SERVER_IP = ip
+    await interaction.response.send_message(f"Теперь указан IP: **{ip}**")
+
+    global server
+    server = JavaServer.lookup(f'{SERVER_IP}:{SERVER_PORT}')
+
+@bot.tree.command(name="port", description="Добавление порта сервера")
+@app_commands.describe(port = "Порт сервера")
+async def port(interaction: discord.Interaction, port: int):
+    global SERVER_PORT
+    SERVER_PORT = port
+    await interaction.response.send_message(f"Теперь указан порт: **{port}**")
+
+    global server
+    server = JavaServer.lookup(f'{SERVER_IP}:{SERVER_PORT}')
 
 bot.run(TOKEN)
